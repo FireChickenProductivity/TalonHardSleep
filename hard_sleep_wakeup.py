21559a175a3909a16f162d01ee882c51e197bff9
FireChickenProductivity <107892169+FireChickenProductivity@users.noreply.github.com>
2023-05-11 12:22:02 -0600

    Have delayed wakeup displayed

--- hard_sleep_wakeup.py
+++ hard_sleep_wakeup.py
@@ -34,12 +34,12 @@ wakeup_delay = module.setting(
 
 wakeup_counter: int = 0
 number_of_availability_blocks: int = 0
-delayed_wakeup_canceled: bool = False
+waking_up: bool = False
 @module.action_class
 class Actions:
     def hard_sleep_wakeup():
         ''''''
-        global wakeup_counter, number_of_availability_blocks
+        global wakeup_counter, number_of_availability_blocks, waking_up
         wakeup_counter += 1
         if wakeup_counter >= wakeups_needed.get() and wakeups_needed.get() != 0:
             if number_of_availability_blocks == 0:
@@ -47,10 +47,13 @@ class Actions:
                     actions.user.hard_sleep_wakeup_immediately()
                 else:
                     def wakeup_if_not_canceled():
-                        global delayed_wakeup_canceled
-                        if not delayed_wakeup_canceled:
+                        global waking_up
+                        if waking_up:
                             actions.user.hard_sleep_wakeup_immediately()
-                        delayed_wakeup_canceled = False
+                        else:
+                            actions.user.hard_sleep_reset_counter()
+                        waking_up = False
+                    waking_up = True
                     cron.after(f'{wakeup_delay.get()}ms', wakeup_if_not_canceled)
             else:
                 wakeup_counter -= 1
@@ -88,8 +91,8 @@ class Actions:
     
     def hard_sleep_cancel_delayed_wakeup():
         ''''''
-        global delayed_wakeup_canceled
-        delayed_wakeup_canceled = True
+        global waking_up
+        waking_up = False
 
 def should_show_hard_sleep_display() -> bool:
     return should_show_display.get() != 0
@@ -99,4 +102,6 @@ def gui(gui: imgui.GUI):
     gui.text('Hard Sleep')
     gui.text(f'Wakeup counter: {wakeup_counter}')
     if number_of_availability_blocks > 0:
-        gui.text(f'Temporary unavailability counter: {number_of_availability_blocks}')
+        gui.text('Wakeup temporarily unavailable!')
+    if wakeup_delay.get() != 0 and waking_up:
+        gui.text('Waking up!')
